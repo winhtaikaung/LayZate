82a2a1b54b71990a7aa20f1e4b2c3ec837ddc07b
Win Htaik Aung(JR) <winhtaikaung28@hotmail.com>
2014-11-26 02:39:01 +0630

    Change Name

--- app/src/main/java/cControls/Custom_Flightlist_Adapter.java
+++ app/src/main/java/cControls/Custom_Flightlist_Adapter.java
@@ -1,6 +1,6 @@
 package cControls; 
  /*          
- * Copyright 2014 Win Htaik Aung
+ * Copyright 2014 Win Htaik Aung(WHA)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
